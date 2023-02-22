a243ccd75d1580446fca0a3dc8eb0041c3c93297
Kevin Paucar <kpaucar05@gmail.com>
2023-02-22 10:52:16 -0600

    database url

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,4 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/EscapeRoom_db');
 
 module.exports = client;
