3e847e21a7f12a058d68df526a775a623a57680d
Kevin Paucar <kpaucar05@gmail.com>
2023-02-23 08:51:38 -0600

    statred tables

--- server/db/index.js
+++ server/db/index.js
@@ -5,17 +5,34 @@ const {
   authenticate
 } = require('./User');
 
+
 const syncTables = async()=> {
-  const SQL = `
-  DROP TABLE IF EXISTS users;
-  CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
-  );
-  `;
-  await client.query(SQL);
-};
+  try{
+    console.log("Dropping any existing tables...");
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS EscapeRooms;
+  `);
+  
+  await client.query(`
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(100) NOT NULL
+    );
+  
+    CREATE TABLE EscapeRooms(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT
+    );
+  `);
+    console.log("Tables created!");
+  } catch (error) {
+    console.error("Error syncing tables:", error);
+    throw error;
+  }
+}
 
 const syncAndSeed = async()=> {
   await syncTables();
