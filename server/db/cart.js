72ebb3e243a637a6d38107912062992bdfc7981e
Alex G <Agoerner16@gmail.com>
2023-02-27 07:53:43 -0700

    added create cart function

---
+++ server/db/cart.js
@@ -0,0 +1,19 @@
+const client = require('./client');
+
+const createCart = async({ buyerId }) {
+    try {
+      const {rows: [cart]} = await client.query(`
+        INSERT INTO cart ("buyerId")
+        VALUES($1)
+        RETURNING *;
+      `, [buyerId]);
+        return cart;
+    } 
+    catch (error) {
+      console.log(error)
+      throw error
+     }
+  }
+
+  module.exports = {
+    createCart }
