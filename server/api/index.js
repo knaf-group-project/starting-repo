b92a764ab670b8cd49654a5f8f542f12c0247bd8
Kevin Paucar <kpaucar05@gmail.com>
2023-02-27 10:38:31 -0600

    seed data routes

---
+++ server/api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// ROUTER: /api/User
+const usersRouter = require('../db/User');
+router.use('/User', usersRouter);
+
+// ROUTER: /api/EscapeRooms
+const EscapeRoomsRouter = require('../db/EscapeRooms');
+router.use('/EscapeRooms', EscapeRoomsRouter);
+
+// ROUTER: /api/cart
+const cartRouter = require('../db/cart');
+router.use('/cart', cartRouter);
+
+// ROUTER: /api/cart_products
+const cartProductsRouter = require('../db/cart_products');
+// const { getUserById } = require('../db');
+router.use('/cart_products', cartProductsRouter);
+
+router.use((error, req, res, next) => {
+    if (error.name == "UnauthorizedUserError") {
+        res.status(403);
+    }
+    res.send({
+      name: error.name,
+      message: error.message
+    });
+  });
+
+module.exports = router;
