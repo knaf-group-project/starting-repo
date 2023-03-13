f08de245076b048f34544e23a75f298c6c3cb4ff
Shan <norman_17899@yahoo.com>
2023-03-13 10:29:36 -0500

    got individual links

--- src/Components/EscapeRoom.js
+++ src/Components/EscapeRoom.js
@@ -5,16 +5,18 @@ const EscapeRoom = (props) => {
 
   const { rooms } = props
   const id = useParams().id;
-  const room = rooms.find(room => room.id === id)
+  const room = rooms.find(room => room.id == id)
+
 
   if (!room) {
     return null;
   }
+
   return (
     <div>
       <div>
-        <h1><Link to={'/EscapeRooms'}>{room.id.name}</Link></h1>
-        <p><span className="postHeaders">Description: </span>{room.description}</p>
+        <h1><Link to={'/EscapeRooms'}>{room.name}</Link></h1>
+        <p><span className="postHeaders">Description: </span>{room.maindescription}</p>
         <p><span className="postHeaders">Time: </span></p>
       </div>
     </div>
