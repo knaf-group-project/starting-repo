0b08a3108e406a09854fd329bab91835f02e41ed
Shan <norman_17899@yahoo.com>
2023-03-22 09:49:08 -0500

    fixed cases

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from './Home'
 export { default as Login } from './Login';
 export { default as EscapeRoom } from './EscapeRoom'
-export { default as NavBar } from './NavBar';
+export { default as Navbar } from './Navbar';
 export { default as Register } from './Register';
 export { default as Footer } from './Footer'
 export { default as Cart } from './Cart'
